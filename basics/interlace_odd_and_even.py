ba9c93be3fc7ac5ad16a8d17c34c8af81083851e
Jeremy7002 <jerejames1.12@gmail.com>
2026-01-02 09:12:53 +0530

    Refactored the INTERLACE code

--- basics/interlace_odd_and_even.py
+++ basics/interlace_odd_and_even.py
@@ -1,37 +1,43 @@
-a=int(input())
-b=int(input())
-odds=[]
-evens=[]
-inter=[]
-for i in range(a,b+1):
-    if i%2!=0:
-        odds.append(i)
-for i in range(b,a-1,-1):
-    if i%2==0:
-        evens.append(i)
-i,j=0,0
-if a%2==0 and b%2==0:
-    while i<len(odds) and j<len(evens):
-        inter.append(evens[j])
-        inter.append(odds[i])
-        i+=1
-        j+=1
-    while j<len(evens):
-        inter.append(evens[j])
-        j+=1
-elif a%2!=0 and b%2!=0:
-    while i<len(odds) and j<len(evens):
-        inter.append(odds[i])
-        inter.append(evens[j])
-        i+=1
-        j+=1
-    while i<len(odds):
-        inter.append(evens[j])
-        i+=1
-else:
-    while i<len(odds) and j<len(evens):
-        inter.append(odds[i])
-        inter.append(evens[i])
-        i+=1
-        j+=1
-print(*inter)
+def get_odds(start, end):
+    return [i for i in range(start, end + 1) if i % 2 != 0]
+
+
+def get_evens(end, start):
+    return [i for i in range(end, start - 1, -1) if i % 2 == 0]
+
+
+def interlace_lists(list1, list2, start_with_first=True):
+    result = []
+    i = j = 0
+
+    while i < len(list1) and j < len(list2):
+        if start_with_first:
+            result.append(list1[i])
+            result.append(list2[j])
+        else:
+            result.append(list2[j])
+            result.append(list1[i])
+        i += 1
+        j += 1
+
+    result.extend(list1[i:])
+    result.extend(list2[j:])
+
+    return result
+
+
+def main():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+
+    odds = get_odds(a, b)
+    evens = get_evens(b, a)
+
+    end_with_odd = b % 2 != 0
+    interlaced = interlace_lists(odds, evens, end_with_odd)
+
+    print(*interlaced)
+
+
+if __name__ == "__main__":
+    main()
